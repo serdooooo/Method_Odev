8eebf25fe4af70e5579df55fca4998e868ffeef6
karakurtserdar19@gmail.com <karakurtserdar19@gmail.com>
2021-11-23 17:49:32 +0300

    new

---
+++ kurs_23112021_45_Method_Example_2/Matematik.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kurs_23112021_45_Method_Example_2
+{
+    class Matematik
+    {
+
+        public static void Menu()
+        {
+            Console.WriteLine("****Menü****");
+            Console.WriteLine("1-Toplama");
+            Console.WriteLine("2-Çıkarma");
+            Console.WriteLine("3-Çarpma");
+            Console.WriteLine("4-Bölme");
+            
+        }
+        public  void toplama(int a,int b)
+        {
+
+            int top = 0;
+            top = a + b;
+            Console.WriteLine("Toplama sonucu: "+top);
+        }
+        public  void cikarma(int a,int b)
+        {
+            int cikar = 0;
+            cikar = a - b;
+            Console.WriteLine("Çıkartma sonucu: "+cikar);
+        }
+        public  void carpma(int a,int b)
+        {
+            int carp = 0;
+            carp = a * b;
+            Console.WriteLine("Çarpma sonucu: "+carp);
+        }
+        public  void bolme(double a,double b)
+        {
+            double bol = 0;
+            bol = a / b;
+            Console.WriteLine("Bölme sonucu: "+bol);
+
+        }        
+    }    
+}
--- kurs_23112021_45_Method_Example_2/Program.cs
+++ kurs_23112021_45_Method_Example_2/Program.cs
@@ -11,6 +11,54 @@ namespace kurs_23112021_45_Method_Example_2
         static void Main(string[] args)
         {
 
+  devamet:          Matematik m1 = new Matematik();
+            Matematik.Menu();
+            
+            Console.Write("Lütfen bir değer seçiniz: ");
+            int deger = Convert.ToInt32(Console.ReadLine());
+
+           
+            do
+            {
+                Console.WriteLine("İşlem yapmak istediğiniz değerleri giriniz");
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                switch (deger)
+            {
+                case 1:
+                    m1.toplama(a,b);
+                    break;
+                case 2:
+                    m1.cikarma(a,b);
+                    break;
+                case 3:
+                    m1.carpma(a,b);
+                    break;
+                case 4:
+                    m1.bolme(a,b);
+                    break;
+                default:
+                        Console.WriteLine("Belirtmiş olduğunuz değer liste içerisinde bulunamadı.");
+                        Console.WriteLine("Lütfen tekrar deneyiniz...");
+                        System.Threading.Thread.Sleep(2000);
+                        goto devamet;
+             }
+                Console.WriteLine("Başka işlem yapmak ister misiniz? e/h");
+                string devam = Console.ReadLine();
+                if (devam == "e")
+                {
+                    goto devamet;
+                }
+                else
+                {
+                    Console.WriteLine("Matematik buraya kadardı...");
+                    System.Threading.Thread.Sleep(2000);
+                    Environment.Exit(0);
+                }
+
+            } while (true);
+            Console.ReadLine();
+
         }
     }
 }
